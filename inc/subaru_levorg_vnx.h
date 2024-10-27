e734644479e0b5b64a065ca089b777acd46adc1a
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2024-10-27 12:15:42 +0900

    Update subaru_levorg_vnx.h

--- inc/subaru_levorg_vnx.h
+++ inc/subaru_levorg_vnx.h
@@ -38,6 +38,10 @@ enum avh_control_status {
 #define AVH_OFF false
 #define AVH_ON  true
 
+// PARKING BRAKE
+#define BRAKE_OFF false
+#define BRAKE_ON  true
+
 // STATUS
 enum status {
     PROCESSING,
