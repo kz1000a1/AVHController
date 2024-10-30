0389968dffb56b47a33c3e4d6f157f864d011799
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2024-10-30 14:34:23 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -328,12 +328,10 @@ int main(void)
                                             } else {
                                                 Retry++;
                                                 // led_blink(Retry);
-                                                /* for(int i = 0;i < 5;i++){
+                                                for(int i = 0;i < 2;i++){
                                                     HAL_Delay(50);
                                                     transmit_can_frame(rx_msg_data, AvhControl); // Transmit can frame for introduce or remove AVH
-                                                } */
-                                                HAL_Delay(50);
-                                                transmit_can_frame(rx_msg_data, AvhControl); // Transmit can frame for introduce or remove AVH
+                                                }
                                                 // Discard message(s) that received during HAL_delay()
                                                 while(is_can_msg_pending(CAN_RX_FIFO0)){
                                                     can_rx(&rx_msg_header, rx_msg_data);
