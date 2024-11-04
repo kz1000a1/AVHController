6c636152e70a5c9be950da2e78b85e0ef795a087
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2024-11-04 09:57:01 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -192,7 +192,7 @@ int main(void)
                         switch (AvhStatus){
                             case AVH_ON:
                                 if(AvhControl == AVH_ON){
-                                    if((Gear != SHIFT_D && (Brake != 0.0 || AvhHold == HOLD_OFF)) || ((Brake == 0.0 || SafetyBelt == BELT_OFF || Door == DOOR_OPEN) && AvhHold == HOLD_OFF) || ParkBrake == BRAKE_ON || Accel != 0.0 || (PrevBrake == 0.0 && Brake != 0.0 && Brake < BRAKE_HIGH)){
+                                    if((Gear != SHIFT_D && (BRAKE_LOW < Brake || AvhHold == HOLD_OFF)) || ((Brake == 0.0 || SafetyBelt == BELT_OFF || Door == DOOR_OPEN) && AvhHold == HOLD_OFF) || ParkBrake == BRAKE_ON || Accel != 0.0 || (PrevBrake == 0.0 && Brake != 0.0 && Brake < BRAKE_HIGH)){
                                         AvhControl = AVH_OFF;
                                         if(Status == SUCCEEDED){
                                             Status = PROCESSING;
