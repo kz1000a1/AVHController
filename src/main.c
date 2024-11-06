535a21302b3ca92065b82499ab62b54d3622e258
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2024-11-06 16:11:40 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -316,7 +316,7 @@ int main(void)
                             } else {
                                 if(Status == CANCELLED || Status == FAILED){
                                     if(Led){
-                                        led_blink(!((AvhStatus << 1) + AvhControl));
+                                        led_blink((!AvhStatus << 1) + !AvhControl);
                                     } else {
                                         led_blink((AvhStatus << 1) + AvhControl);
                                     }
