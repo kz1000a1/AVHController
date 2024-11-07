d2a3c3e12c5f57a94317f9d04f0477ab90125468
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2024-11-07 16:15:31 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -268,7 +268,7 @@ int main(void)
                     break;
 
                 case CAN_ID_AVH_STATUS:
-                    if(((((rx_msg_data[5] & 0x20) == 0x20) ^ VnxParam.AvhStatus) && (rx_msg_data[5] & 0x20) == 0x20)) || ((((rx_msg_data[5] & 0x22) == 0x22) ^ VnxParam.AvhHold) && (rx_msg_data[5] & 0x22) == 0x22)){
+                    if(((((rx_msg_data[5] & 0x20) == 0x20) ^ VnxParam.AvhStatus) && ((rx_msg_data[5] & 0x20) == 0x20)) || ((((rx_msg_data[5] & 0x22) == 0x22) ^ VnxParam.AvhHold) && ((rx_msg_data[5] & 0x22) == 0x22))){
                         dprintf_("# DEBUG AVH: %d(0:OFF,1:ON)=>%d / HOLD: %d=>%d\n", VnxParam.AvhStatus, (rx_msg_data[5] & 0x20) == 0x20, VnxParam.AvhHold, (rx_msg_data[5] & 0x22) == 0x22);
                     }
                     VnxParam.AvhHold = ((rx_msg_data[5] & 0x22) == 0x22);
