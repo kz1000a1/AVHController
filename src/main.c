d6d08b8d9cdc52129b7ba85d155b5dde6f383530
kz1000a1 <146339266+kz1000a1@users.noreply.github.com>
2025-01-12 21:26:53 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -288,7 +288,7 @@ int main(void)
                     print_rx_frame(&rx_msg_header, rx_msg_data);
                     printf_("Switch:%d(%d) Acc:%d(%d) Ready:%d(%d) Hold:%d(%d)\n", VnxParam.EyeSight.Switch, PrevEyeSight.Switch, VnxParam.EyeSight.Acc, PrevEyeSight.Acc, VnxParam.EyeSight.Ready, PrevEyeSight.Ready, VnxParam.EyeSight.Hold, PrevEyeSight.Hold);
 #endif
-                    if(PrevEyeSight.Acc == ON && VnxParam.EyeSight.Acc == OFF && PrevEyeSight.Ready == ON && VnxParam.EyeSight.Ready == OFF && PrevEyeSight.Hold == HOLD && VnxParam.EyeSight.Hold == UNHOLD){
+                    if(PrevEyeSight.Ready == ON && VnxParam.EyeSight.Ready == OFF && PrevEyeSight.Hold == HOLD && VnxParam.EyeSight.Hold == UNHOLD){
                         if(OffByBrake == OFF){
                             OffByBrake = ON;
                             dprintf_("# DEBUG ByBrake:%d(0:OFF,1:ON)\n", OffByBrake);
